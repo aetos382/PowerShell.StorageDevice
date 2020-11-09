5c2ff6c425d68b7834bc54e2ecc4d6094a63d05f
aetos <aetos@aerie.jp>
2020-11-10 03:23:59 +0900

    LocalizableResourceGenerator の実験

---
+++ LocalizableResourceStringGenerator/Generator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.CodeAnalysis;
+
+using System.Diagnostics;
+
+namespace LocalizableResourceStringGenerator
+{
+    [Generator]
+    public class Generator :
+        ISourceGenerator
+    {
+        public void Execute(
+            GeneratorExecutionContext context)
+        {
+            foreach (var additionalFile in context.AdditionalFiles)
+            {
+                Debug.WriteLine(additionalFile.Path);
+            }
+        }
+
+        public void Initialize(
+            GeneratorInitializationContext context)
+        {
+            System.Diagnostics.Debugger.Launch();
+        }
+    }
+}
