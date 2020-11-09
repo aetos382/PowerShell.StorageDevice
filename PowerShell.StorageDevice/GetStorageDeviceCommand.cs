55f546d83ea72ce77ee147a0552893bd2e08e25d
aetos <aetos@aerie.jp>
2020-11-10 01:36:27 +0900

    適当な実装

--- PowerShell.StorageDevice/GetStorageDeviceCommand.cs
+++ PowerShell.StorageDevice/GetStorageDeviceCommand.cs
@@ -1,13 +1,36 @@
 ﻿using System.Management.Automation;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Windows.Devices.Enumeration;
+
+using PSAsync;
+using PSAsync.Command;
 
 namespace PowerShellStorageDevice
 {
     [Cmdlet(VerbsCommon.Get, "StorageDevice")]
+    [OutputType(typeof(DeviceInformation))]
     public class GetStorageDeviceCommand :
-        Cmdlet
+        Cmdlet,
+        IAsyncCmdlet
     {
         protected override void ProcessRecord()
         {
+            this.ExecuteAsyncAction((c, t) => c.ProcessRecordAsync(t));
+        }
+
+        public async Task ProcessRecordAsync(
+            CancellationToken cancellationToken)
+        {
+            var devices = StorageDeviceManager.GetStorageDevices();
+
+            await foreach (var device in devices)
+            {
+                await this
+                    .WriteObjectAsync(device, cancellationToken)
+                    .ConfigureAwait(false);
+            }
         }
     }
 }
